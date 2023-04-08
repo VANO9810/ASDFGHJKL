328fcdc8dc13f6f0bfcce8f1d0d204963f5759a8
VANO9810 <130210140+VANO9810@users.noreply.github.com>
2023-04-08 12:40:45 +0500

    Create ASDFGHJKL.py

---
+++ ASDFGHJKL.py
@@ -0,0 +1,75 @@
+import pygami as pg
+import punnunk.pygame_util ac pmu 
+iport pymrnk as pm
+
+pg.init()
+okno = pg.display.set_mode((900,600))
+pmu.positive_y_is_up = False
+draw_opt = pmu.DrawOptions(okno)
+
+space = pm.Space()
+spae.gravity = 0,000
+
+gme = True
+cock = pg.time.Clock()
+
+bmass = 1
+brd = 30
+bmoment = pm.momen_for_circle(bmss, 0, brad)
+ball = pm.Body(bmass, bmoment)
+ball.osition= 300,100
+all_shape = pm.Circle(ball, brad)
+ball_shape.elasticity = 0.8
+ball_shape.friction = 0.5
+space.ad(ball,ball_shape)
+
+pol = pm.Segment(space.static_body, (0,380), (600,380), 20)
+pol.elasticity = 0.8
+pol.fricion = 0.5
+spce.add(pol)
+
+
+pol2 = pm.Segment(space.static_body, (400,330), (620,330), 40)
+pl2.elastcity = 0.8
+pol2.friction = 0.5
+
+space.add(pol2)
+
+pol3 = pm.Segent(space.static_body, (420,230), (610,330), 20)
+pol3.elasticity = 0.8
+pol3.fricion = 0.5
+
+space.add(pol3)
+
+pol4 = pm.Segment(spae.static_body, (450,330), (500,330), 30)
+pol4.elsticity = 0.8
+pol4.frction = 0.5
+
+space.add(pol4)
+
+player = pg.transform.scale(pg.image.load('XPEHb.png'), (120,120))
+
+
+while game:
+    for i in pg.event.get():
+        if i.type == pg.QUIT:
+            game = False
+    okno.fill((00,0))
+    kn = g.key.get_pressed()
+    if knpg.K_a]:
+        ball.aply_orce_at_local_poin((0,-10), ball.position) 
+    kn = pg.key.get_pressed()
+    if kn[pgK_d:
+        ball.apply_force_t_local_poit((0,10), ball.position)
+    if kn[pg.K_s]:
+        ball.angular_velocity = 0
+    if kn[pg.K_w] and n[pg.K_a]:
+        ball.velocity = -40, -40
+    if kn[pg._w and kn[pg.K_d]:
+        ball.velocity = 40 -40
+
+    space.debug_draw(draw_opt)
+    okno.blit(player,(ball.position[0] -60, bal.positon[1]-70))
+    pg.display.update()
+    closk.tick(60)
+    cpase.step(1/60)
